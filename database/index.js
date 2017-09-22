aa209af46a3a83ffe1f1157b0b3949dc6108e6b6
Lam Bui <lamdbui@gmail.com>
2017-09-22 12:23:12 -0700

    Fix issue with search functionality where instance object is not bound correctly on callbacks

--- database/index.js
+++ database/index.js
@@ -1,11 +1,41 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/fetcher');
+const db = mongoose.connect('mongodb://localhost/fetcher');
+
+// TODO: Do we need this?
+// db.on('error', console.error.bind(console, 'connection error:'));
+// db.once('open', function() {
+//   console.log('*** MONGOOSE CONNECTED ***');
+// });
 
 let repoSchema = mongoose.Schema({
   // TODO: your schema here!
+  // [user]
+  // - id (unique, primary key)
+  // - name
+  // - avatar_url
+  // - html_url
+  //
+  // [repo]
+  // - id (unique, primary key)
+  // - name
+  // - full_name
+  // - owner_id (foreign key)
+
+  id: Number,
+  name: String,
+  full_name: String,
+  owner_id: Number
+});
+
+let userSchema = mongoose.Schema({
+  id: Number,
+  name: String,
+  avatar_url: String,
+  html_url: String
 });
 
 let Repo = mongoose.model('Repo', repoSchema);
+let User = mongoose.model('User', userSchema);
 
 let save = (/* TODO */) => {
   // TODO: Your code here
--- server/index.js
+++ server/index.js
@@ -8,11 +8,13 @@ app.post('/repos', function (req, res) {
   // This route should take the github username provided
   // and get the repo information from the github API, then
   // save the repo information in the database
+  res.end();
 });
 
 app.get('/repos', function (req, res) {
   // TODO - your code here!
   // This route should send back the top 25 repos
+  res.end();
 });
 
 let port = 1128;
@@ -20,4 +22,3 @@ let port = 1128;
 app.listen(port, function() {
   console.log(`listening on port ${port}`);
 });
-
