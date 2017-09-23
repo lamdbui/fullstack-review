413414ac4b491fc2d44e0bc41f604fcdd667d87f
Lam Bui <lamdbui@gmail.com>
2017-09-22 17:08:48 -0700

    Tweak database to use a single table for simplicity and now saving data into DB

--- database/index.js
+++ database/index.js
@@ -20,11 +20,14 @@ let repoSchema = mongoose.Schema({
   // - name
   // - full_name
   // - owner_id (foreign key)
+  // TODO: remove this later and reference the User table
+  // - owner_name
 
   id: Number,
   name: String,
   full_name: String,
-  owner_id: Number
+  owner_id: Number,
+  owner_name: String
 });
 
 let userSchema = mongoose.Schema({
@@ -37,10 +40,33 @@ let userSchema = mongoose.Schema({
 let Repo = mongoose.model('Repo', repoSchema);
 let User = mongoose.model('User', userSchema);
 
-let save = (/* TODO */) => {
+let save = (repoArr, callback) => {
+
   // TODO: Your code here
   // This function should save a repo or repos to
   // the MongoDB
+  let saveAttemptCount = repoArr.length;
+  let saveCount = 0;
+  repoArr.forEach(repo => {
+    // save it!
+    let repoModel = new Repo(repo);
+    console.log('repoObj:', repoModel);
+    repoModel.save((error, resultObj, numAffected) => {
+      if (error) {
+        console.log('[database:error]', error);
+        // callback(error, 0);
+      } else {
+        console.log(`[database:resultObj : numAffected] ${resultObj} : ${numAffected}`);
+        // TODO: return the number of rows changed
+        // callback(null, numAffected);
+        saveCount++;
+      }
+      saveAttemptCount--;
+      if (saveAttemptCount === 0) {
+        callback(saveCount);
+      }
+    });
+  });
 }
 
 module.exports.save = save;
--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const github = require('../helpers/github');
+const database = require('../database');
 let app = express();
 
 app.use(express.static(__dirname + '/../client/dist'));
@@ -12,23 +13,53 @@ app.post('/repos', function (req, res) {
   console.log('/repos POST');
 
   req.on('data', data => {
+    // TODO: Should we do input validation here?
     let username = data.toString();
     console.log('*** DATA:', username);
-    // TODO: Should we do validation here?
     github.getReposByUsername(username, (error, result) => {
       if (error) {
         console.log('*** GET REPO ERROR:', error);
+        // TODO: Relay some sort of failure back to the client
         res.end();
       } else {
         // console.log('*** GET REPO SUCCESS:', result);
-        let githubFetchResult = result.body;
-        console.log('*** GET REPO SUCCESS:', githubFetchResult);
-        res.end();
+        let githubFetchResult = JSON.parse(result.body);
+        // console.log('*** GET REPO SUCCESS:', githubFetchResult);
+        console.log('*** GET REPO SUCCESS:', githubFetchResult.length);
+        //   id: Number,
+        //   name: String,
+        //   full_name: String,
+        //   owner_id: Number
+        // });
+        //
+        // let userSchema = mongoose.Schema({
+        //   id: Number,
+        //   name: String,
+        //   avatar_url: String,
+        //   html_url: String
+        //
+        // let repoArr = [];
+        // githubFetchResult.forEach(result => {
+        //   repoArr
+        // });
+        let repoModelArr = githubFetchResult.map(result => {
+          return {
+            id: result.id,
+            name: result.name,
+            full_name: result.full_name,
+            owner_id: result.owner.id,
+            owner_name: result.owner.login
+          };
+        });
+        console.log('*** REPO MODELS: ', repoModelArr);
+        database.save(repoModelArr, numSaved => {
+          console.log('[server] SAVED RECORDS =', numSaved);
+          res.end();
+        });
+        // res.end();
       }
     });
   });
-
-  // res.end();
 });
 
 app.get('/repos', function (req, res) {
